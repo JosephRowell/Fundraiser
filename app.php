e290c113134b940b3cd646da0fca427cd9166c9b
JosephRowell <rowelljoey@yahoo.com>
2014-03-06 15:13:46 -0500

    Updated Fundraiser App
    Needs to count more correctly. Also clone address text fields when "+" is clicked.

--- app.php
+++ app.php
@@ -1,19 +1,17 @@
 <!DOCTYPE>
 <html>
-	<head>
-		<meta http-equiv="content-type" content="text/xml;
+<head>
+	<meta http-equiv="content-type" content="text/xml;
 		charset=utf-8" />
 		<link rel = "stylesheet" type="text/css" href="sauce.css" />
 		<script src ="formjs.js"></script>
 		 <script src="jquery-1.10.2.min.js"></script>
 		 <script src="jquery-ui-1.10.3\ui\jquery-ui.js"></script>
-		 <script src ="bigone.js"></script>
+		<!-- <script src ="bigone.js"></script> -->
 
 		<title>TURF LOG </title>
-
-	</head>
- <body>
-	 
+</head>
+<body>
 <h1> TURF LOG </h1>
 
 <div id ="cleartop">
@@ -24,16 +22,15 @@
    <form action = "better.php"
 		method = "post">
 		
-		Canvasser: <input type="text" name="Canvasser" value="" size="12" min="1" max="30" /><br />
-        Field Manager:  <input type="text" name="Field_Manager" value="" size="12" min="0" max="30" /><br />
+		Canvasser: <input type="text" name="Canvasser" value="" size="40" min="1" max="30" /><br />
+        Field Manager:  <input type="text" name="Field_Manager" value="" size="40" min="0" max="30" /><br />
 		Municipality: <input type="text" name="Municipality" value="" size="20" min="0" max="20" /><br />
 		Day: <input type="text" name="Day" value="<?php echo date("D-m-d-Y")?>" /><br />
-		Zip: <input type="text" name="Zip" size="5" min="0" max="999999" /> <br />
+		Zip: <input type="text" name="Zip" size="5" pattern="^[0-9]{5}$" /> <br />
 		
 		<input type = "submit">
-		</form>
-
-   </p>
+	</form>
+	 </p>
   </div>
 
   <div id = "rightdata">
@@ -46,39 +43,29 @@
 		<strong>Yes-C</strong> = Yes, gave a contribution (write amount in Result box)<br />
 		<strong>X</strong> = Not interested, or refused to contribute<br />
 
-		
     </p> 
    </div>
 </div>
-
-<div id = "travel">
-		<form action = " ">
-			<fieldset>
-				<label>Travel: </label>
-					<input type = "number" size="5" max="250"
-					id = "mileage"  />
-		<button type = "button"
-		onclick = "calcGas()">
-	Get Gas
-		</button> <!-- EXPERIMENTAL STUFF! NONE OF THIS ACTUALLY DOES ANYTHING YET! -->
-				<label>R/A</label>
-					<input type = "text"
-					id = "ra" />
-				<label>Can</label>
-					<input type = "number" size="3" min="0" max="200"
-					id = "can" />
+	<div class="travel">
+    <form action=" ">
+        <fieldset>
+            <label>Travel:</label>
+            <input type="number" size="3" max="200" id="mileage" name="mileage" />
+            <button type="button" onclick="calcGas()">Get Gas</button>
+			
+			<!-- EXPERIMENTAL STUFF! NONE OF THIS ACTUALLY DOES ANYTHING YET! -->
 				<label>Cash</label>
 					<input type = "text" size="5" min="0"
-					id = "cash" />
+					id = "cash" name = "cash" />
 				<label>Check</label>
 					<input type = "text" size="5" min="0"
-					id = "check" />
+					id = "check" name = "check" />
 				<label>CD</label>
 					<input type = "text" size="5" min="0"
-					id = "cd" />
+					id = "cd" name = "cd" />
 				<label>Total<label>
 					<input type = "text" size="6" min="0"
-					id = "dailytotal" />
+					id = "dailytotal" name = "dailytotal" />
 		                     <!-- -->
 			</fieldset>
 		</form>
@@ -88,79 +75,181 @@ Watch space
    </div>
 </div>
 
-<div id= "main">
-<script type="text/javascript"> <!-- THIS IS WHERE ALL THE MAGIC HAPPENS! -->
-function addRow(r) {
-    var root = r.parentNode; //the root
-    var allRows = root.getElementsByTagName('tr'); //the rows' collection
-    var cRow = allRows[0].cloneNode(true); //the clone of the 1st row
-    var cInp = cRow.getElementsByTagName('input'); //the inputs' collection of the 1st row
-    for (var i = 0; i < cInp.length; i++) { //changes the inputs' names (indexes the names)
-        cInp[i].setAttribute('name', cInp[0].getAttribute('name') + '_' + (allRows.length + 1));
-    }
-    var cSel = cRow.getElementsByTagName('select')[0];
-    cSel.setAttribute('name', cSel.getAttribute('name') + '_' + (allRows.length + 1)); //change the selecet's name
-    root.appendChild(cRow); //appends the cloned row as a new row
-}
-<!-- This code "works" but what it really needs is some error checking to make sure you can't delete the first row. -->
-
-function delRow(node) {
-    node.parentNode.removeChild(node);
-}
-
-<!-- Still working on this function because I don't like the address number getting repeated -->
-function clearaddy() {
-	space = document.getElementById("Number");
-	if (space != ""){
-	space = "";
-	}
-}
-
-</script>
-
-<form action="" method="get">
-    <table width="766" border="0" cellspacing="0" cellpadding="0">
+<table id="dynatable" class="thelist"> <!--if I needed to start over, I'd do it from here -->
+    <thead>
         <tr>
-            <td width="180">
-                <input type="number" name="Number" size="5" placeholder="#" min="0" max="99999" value="" />
-            </td>
-            <td width="180">
-                <input type="text" name="Street" placeholder="StreetName" max="25" />
-            </td>
-            <td width="180">
-                <select id="dropage">
-                    <option value="" selected="selected">Select Value</option>
+            <th>Number</th>
+            <th>Address</th>
+            <th>Code</th>
+            <th>Amount</th>
+        </tr>
+    </thead>
+    <tbody class="tbod"> <!-- Append function starts here -->
+        <tr>
+            <td><input type="text" class="snumber" placeholder="#" name="snumber" autofocus  /> </td>
+            <td><input type="text" class="address" placeholder="StreetName" name="address"     /> </td>
+            <td>
+                <select name="turfcode" class="turfcode">
+                    <option value="">SELECT</option>
                     <option value="NH">NH</option>
                     <option value="CB">CB</option>
-                    <option value="Yes-R">Yes-R</option>
-                    <option value="Yes-NM">Yes-NM</option>
-                    <option value="Yes-C">Yes-C</option>
-                    <option value="X">X</option>
-                    <td width="191">
-                        <input type="number" name="Amount" size = "7" placeholder="Amount" min = "0" max = "10000" />
-                    </td>
+					<option value="X">X</option>
+					<option value="YES-R">YES-R</option>
+					<option value="YES-NM">YES-NM</option>
+					<option value="YES-C">YES-C</option>
                 </select>
             </td>
-            <td width="286">
-                <input name="button" type="button" value="+" onclick="addRow(this.parentNode.parentNode);clearaddy();"> <!--remember to insert init(); when bigone.js is done -->
-				<input name="button" type="button" value="-" onclick="delRow(this.parentNode.parentNode);">
-            </td>
+            <td><input type="text" name="amount" placeholder="Amount" /> </td>
+
+			<!-- <td> <button class="addRow" id="addRow">+</button> </td> -->
+			<td> <button class="remRow">-</button> </td>
+
         </tr>
-    </table>
-    <br />
-    <br />
-    <input name="" type="submit" value="Submit" />
+    </tbody> 
+	<tfoot>
+		<tr>
+            <td colspan="5" style="text-align: left;">
+                <input type="button" class="addRow" value="Add Row" />
+            </td>
+        </tr>  
+			
+		<tr>
+			<td colspan="">Amount Raised: $ <span id="totalnight"></span>   </td>
+			<td colspan="">Homes: <span id="Homes"></span>  </td>
+			<td colspan="">Callbacks: <span id="Callbacks"></span>  </td>
+			<td colspan="">Completes: <span id="Completes"></span>  </td>
+		</tr>
+	</tfoot>
+</table>
+<script>
+$(document).ready(function() {
+	$(".remRow").hide();
+	$("input[autofocus]").focus(); //move cursor to number
+	
+	//counts
+	counter = 0;
+	counter = $('#dynatable tr').length -2; // -2 because of addRow button and footer
+	
+	calls = 0;
+    	calls = $('#dynatable tr').length -2;
+	
+	completes = 0;
+    	completes = $('#dynatable tr').length -2;
+	
+	//turfval = $(this).closest('.turfcode').val()
+	// 
+	pp = $(this).closest('tr').prev().prev().children('.turfcode').val();
+	pp2 = $(this).parent().prev('.turfcode').val();
+	pp3 = $(this).parent().prev('tr').val();
+	
+		$('.turfcode').change(function() {   //Commented lines below for Debugging.
+	//var tv = $(this).val();
+  
+ //calcCB( $('.addRow').prev('tr')[0] )
+	//calcCB( $(this).closest("tr") ) //This 'works' but not quite what I want.
+	//calcCB( $(this).closest('.turfcode').val() )
+	//calcCB( row.prev() )
+	//calcCB(turfval);
+	//calcCB( pp2 );
+   //calcCB( $(this).val() );
+   //calcCB($(this).row);
+   //alert (tv);
+});    
 
-    <!-- Math checks!!!! **********PROBABLY GOING TO USE PHP PRINTS INSTEAD-->
-    <div id="CBOutput">Callbacks</div>
-    <div id="NHOutput">Not Homes</div>
-    <div id="YESROutput">Renewals</div>
-    <div id="YESCOutput">Yes Contributors</div>
-    <div id="YESNMOutput">New Members</div>
-    <div id="XOutput">Refusals</div>
-	<div id="Grandtotal">Total</div>
-</form>
-</div> <!-- Ending div for main -->
 
-  </body>
- </html>
+$(".addRow").on("click", function () {
+
+	    var newRow = $("<tr>");
+        	var cols = "";
+		
+		
+		cols += '<td><input type="text" placeholder="#" name="snumber' + counter +  '" /> </td>';
+        	
+        	cols += '<td><input type="text" placeholder="StreetName" class="address" name="address' + counter +  '"   /> </td>';
+		
+		cols += '<td><select name="turfcode' + counter + '" ><option value="">SELECT</option> <option value="NH">NH</option><option value="CB">CB</option><option value="X">X</option><option value="YES-R">YES-R</option><option value="YES-NM">YES-NM</option><option value="YES-C">YES-C</option></select></td>';
+		
+        	cols += '<td><input type="text" placeholder="Amount" name="amount' + counter + '" /> </td>';
+		
+		//cols += '<td> <button class="addRow">+</button> </td>';
+		cols +=	'<td> <button class="remRow">-</button> </td>';  
+		//calcCB($(this).closest("tr") ) //'works' but not the way I want it to.
+		calcCB( $('.addRow').prev('tr') ) 
+		
+		
+		
+	$('.turfcode').on ("change", function() {
+	//TODO: Debugging
+});  
+		
+		
+        newRow.append(cols);
+        jQuery("table.thelist").append(newRow);
+        counter++;
+	$("#Homes").text((counter)-1); //display Homes in the designated spot.
+	jQuery('html,body').animate({scrollTop:4000});
+		
+		
+		
+    }); //END ON CLICK
+	
+    $("table.thelist").on("change", 'input[name^="amount"]', function (event) {  //magical amounts
+        calculateRow($(this).closest("tr"));
+        calculateGrandTotal();	
+    });
+    
+    $("table.thelist").on("click", ".remRow", function (event) {
+        $(this).closest("tr").remove();
+        calculateGrandTotal();
+        counter --;
+	$("#Homes").text((counter)-1);	
+    });                                                                             
+	
+
+	
+  }); //END READY
+  
+
+
+function calculateRow(row) {
+	var price = +row.find('input[name^="amount"]').val();
+	
+}
+
+function calculateGrandTotal() { 
+	var grandTotal = 0;
+	$("table.thelist").find('input[name^="amount"]').each(function () {
+		grandTotal += +$(this).val();
+	});
+	$("#totalnight").text(grandTotal.toFixed(2)); 
+}
+
+
+function calcCB(row) { 
+	var vala = row.find('.turfcode').val(); 
+	var valb = row.find('.turfcode').parent().prev('.turfcode');                    
+	
+	alert('i selected: ' + valb +" " + vala + " " +pp+ " " +pp2 + " " + pp3 ); //Debugging
+		if (vala == 'CB' || vala == 'NH'){
+			alert('doing it right!')
+			$("#Callbacks").text((calls)-1);
+			calls++;
+		} else if(vala !== 'CB' || vala !== 'NH')
+			{
+			alert('try again')
+			$("#Completes").text((completes)-1);
+			completes++;
+			}		
+				alert(vala); 
+			}
+
+//Test out some CSS
+/*
+            $("input").css("outline-color", "#ACAB99");
+*/
+
+ 
+</script> 
+
+</body>
+</html>
